0992edafc35241988983e40e3468d21e50ce667b
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2023-07-17 00:16:01 +0300

    did it?

---
+++ js/mobile-menu copy.js
@@ -0,0 +1,17 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle('is-hidden');
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-hidden');
+  });
+})();
